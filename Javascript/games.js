e33b9dd176aedc232e6bb7dd93a398575f16bea9
YousofAbdelrehim <157152463+YousofAbdelrehim@users.noreply.github.com>
2024-02-12 21:33:23 -0500

    Test Game Push

---
+++ Javascript/games.js
@@ -0,0 +1,4 @@
+function onevone() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+}
