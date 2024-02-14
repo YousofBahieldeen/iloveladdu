8efb76cc27d2b06f1c0eea4f791bcf087baa3fe0
YousofAbdelrehim <157152463+YousofAbdelrehim@users.noreply.github.com>
2024-02-13 20:56:32 -0500

    More games

--- Javascript/games.js
+++ Javascript/games.js
@@ -1,15 +1,15 @@
 function onevone() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://book-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/mind4ur/WPpluginsync@main/lib/edit/up.xml" frameborder="0" allowfullscreen></iframe></html>');
 }
 function fnaf() {
     var newWindow = window.open('about:blank', '_blank');
     newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/fnaf1.html" frameborder="0" allowfullscreen></iframe></html>');
 }
 
-function Minecraft() {
+function TimeShooter() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/t3.html" frameborder="0" allowfullscreen></iframe></html>');
 }
 
 function Subway() {
@@ -28,15 +28,15 @@ function BasketBall() {
 }
 function SoccerRandom() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://stickman-hook.io/iframe/index.html" frameborder="0" allowfullscreen></iframe></html>');
 }
 function Stickman() {
     var newWindow = window.open('about:blank', '_blank');
     newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
 }
-function TicTeo() {
+function FunnyShooter() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://html5.gamedistribution.com/rvvASMiM/a1b7ac4025214a5cb0bd01a4f675513a/index.html?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2h0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tL2ExYjdhYzQwMjUyMTRhNWNiMGJkMDFhNGY2NzU1MTNhLyIsInBhcmVudERvbWFpbiI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidG9wRG9tYWluIjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJoYXNJbXByZXNzaW9uIjpmYWxzZSwibG9hZGVyRW5hYmxlZCI6dHJ1ZSwiaG9zdCI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidmVyc2lvbiI6IjEuNS4xNyJ9" frameborder="0" allowfullscreen></iframe></html>');
 }
 function BasketBall() {
     var newWindow = window.open('about:blank', '_blank');
