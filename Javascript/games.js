fdecd2b4680362a5d39e80cc2cfaacd127588ebd
YousofAbdelrehim <157152463+YousofAbdelrehim@users.noreply.github.com>
2024-02-13 19:51:02 -0500

    BIg Change

--- Javascript/games.js
+++ Javascript/games.js
@@ -2,3 +2,47 @@ function onevone() {
     var newWindow = window.open('about:blank', '_blank');
     newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
 }
+function fnaf() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/fnaf1.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+
+function Minecraft() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+}
+
+function Subway() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+}
+
+function SnowRider() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/timeshooter3.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+
+function BasketBall() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+function SoccerRandom() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+function Stickman() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+function TicTeo() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+function BasketBall() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
+function BasketBall() {
+    var newWindow = window.open('about:blank', '_blank');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+}
