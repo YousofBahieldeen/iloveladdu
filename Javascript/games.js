40a86e39f9edffc45a18b5f96215cd8076b6a49f
YousofAbdelrehim <157152463+YousofAbdelrehim@users.noreply.github.com>
2024-02-14 19:51:09 -0500

    Games fix

--- Javascript/games.js
+++ Javascript/games.js
@@ -14,7 +14,7 @@ function TimeShooter() {
 
 function Subway() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/1v1lol" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://classroom.lol/files/SubwaySurfers" frameborder="0" allowfullscreen></iframe></html>');
 }
 
 function SnowRider() {
@@ -28,21 +28,17 @@ function BasketBall() {
 }
 function SoccerRandom() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://stickman-hook.io/iframe/index.html" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/soccerrandom.html" frameborder="0" allowfullscreen></iframe></html>');
 }
 function Stickman() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://stickman-hook.io/iframe/index.html" frameborder="0" allowfullscreen></iframe></html>');
 }
 function FunnyShooter() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://html5.gamedistribution.com/rvvASMiM/a1b7ac4025214a5cb0bd01a4f675513a/index.html?gd_zone_config=eyJwYXJlbnRVUkwiOiJodHRwczovL2h0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tL2ExYjdhYzQwMjUyMTRhNWNiMGJkMDFhNGY2NzU1MTNhLyIsInBhcmVudERvbWFpbiI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidG9wRG9tYWluIjoiaHRtbDUuZ2FtZWRpc3RyaWJ1dGlvbi5jb20iLCJoYXNJbXByZXNzaW9uIjpmYWxzZSwibG9hZGVyRW5hYmxlZCI6dHJ1ZSwiaG9zdCI6Imh0bWw1LmdhbWVkaXN0cmlidXRpb24uY29tIiwidmVyc2lvbiI6IjEuNS4xNyJ9" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://google-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/wergboy/Meteor@8f6f4282e1304fa03b4ea2cdb326184b11655a90/examples/react-in-blaze/fs2.xml" frameborder="0" allowfullscreen></iframe></html>');
 }
-function BasketBall() {
+function slope() {
     var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
-}
-function BasketBall() {
-    var newWindow = window.open('about:blank', '_blank');
-    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://shardnetworkstorage.w3spaces.com/games/basketstars.html" frameborder="0" allowfullscreen></iframe></html>');
+    newWindow.document.write('<!DOCTYPE html><html><style>body {margin:0;overflow:hidden;}iframe {width:100vw;height:100vh;border:none;}</style><iframe src="https://google-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/sk1bx/version@843c1a10f4bf78019b513ea7a8a8c3146f3dfa59/2slope.xml" frameborder="0" allowfullscreen></iframe></html>');
 }
